009ebefb1a455285bdc90110d25eb4f4070ca721
0xNineteen <0x39015319@gmail.com>
2022-07-07 15:49:26 -0400

    poc for settle bug

--- settle_test.py
+++ settle_test.py
@@ -7,17 +7,16 @@ sys.path.insert(0, './driftpy/src/')
 sys.path.insert(0, '../driftpy/src/')
 sys.path.insert(0, '../')
 
-import driftpy
 import os 
 import datetime
 import math 
 
+import driftpy
 from driftpy.math.amm import *
 from driftpy.math.trade import *
 from driftpy.math.positions import *
 from driftpy.math.market import *
 from driftpy.math.user import *
-
 from driftpy.types import *
 from driftpy.constants.numeric_constants import *
 
@@ -25,7 +24,6 @@ import json
 import matplotlib.pyplot as plt 
 import numpy as np 
 import pandas as pd
-from dataclasses import dataclass, field
 from driftpy.math.amm import calculate_price
 
 from programs.clearing_house.math.pnl import *
@@ -36,6 +34,8 @@ from programs.clearing_house.lib import *
 from sim.helpers import random_walk_oracle, rand_heterosk_oracle, class_to_json
 from sim.events import * 
 from sim.agents import * 
+
+np.random.seed(0)
 
 def setup_ch(base_spread=0, strategies='', n_steps=100, n_users=2):
     prices, timestamps = random_walk_oracle(1, n_steps=n_steps)
@@ -57,7 +57,7 @@ def setup_ch(base_spread=0, strategies='', n_steps=100, n_users=2):
     for i in range(n_users):
         ch = DepositCollateralEvent(
             user_index=i, 
-            deposit_amount=1_000 * QUOTE_PRECISION, 
+            deposit_amount=1_000_000 * QUOTE_PRECISION, 
             timestamp=ch.time,
         ).run(ch)
 
@@ -68,8 +68,8 @@ ch = setup_ch(n_users=2)
 market = ch.markets[0]
 market.amm.total_fee_minus_distributions = 0 
 
-ch.add_liquidity(0, 0, 1_000 * QUOTE_PRECISION)
-ch.add_liquidity(0, 1, 1_000 * QUOTE_PRECISION)
+ch.add_liquidity(0, 0, 1_000_000 * QUOTE_PRECISION)
+ch.add_liquidity(0, 1, 1_000_000 * QUOTE_PRECISION)
 
 market.amm.total_fee_minus_distributions = 100 
 
@@ -78,6 +78,11 @@ print(market.amm.total_fee_minus_distributions)
 ch.settle_lp(0, 1)
 print(market.amm.total_fee_minus_distributions)
 
+print(
+    "market_fee, user_fee:",
+    market.amm.total_fee_minus_distributions, 
+    ch.users[0].positions[0].last_total_fee_minus_distributions,
+)
 og_collateral = ch.users[0].collateral
 ch.settle_lp(0, 0)
 post_settle_collateral = ch.users[0].collateral
