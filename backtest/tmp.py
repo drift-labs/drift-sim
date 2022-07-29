fcce79b3d279b0e7e16ab7604e90f767a0bb4bde
0xNineteen <0x39015319@gmail.com>
2022-07-29 08:16:04 -0400

    bug fix

--- backtest/tmp.py
+++ backtest/tmp.py
@@ -33,56 +33,7 @@ from driftpy.accounts import get_user_account
 from anchorpy import Provider, Program, create_workspace
 from programs.clearing_house.state.market import SimulationAMM, SimulationMarket
 
-path = '../driftpy/protocol-v2'
-workspace = create_workspace(path)
-program: Program = workspace["clearing_house"]
-oracle_program: Program = workspace["pyth"]
-provider: Provider = program.provider
-
-async def setup(init_market: SimulationMarket):
-    # init usdc mint
-    usdc_mint = await _usdc_mint(provider)
-
-    # init state + bank + market 
-    clearing_house = Admin(program)
-    await clearing_house.initialize(usdc_mint.public_key, True)
-    await clearing_house.initialize_bank(usdc_mint.public_key)
-
-    amm = init_market.amm
-    init_price = round(amm.peg_multiplier / np.log10(PEG_PRECISION), 3)
-    oracle = await mock_oracle(workspace["pyth"], init_price, -int(np.log10(PEG_PRECISION)))
-    await clearing_house.initialize_market(
-        oracle, 
-        int(amm.base_asset_reserve), 
-        int(amm.quote_asset_reserve), 
-        int(amm.funding_period), 
-        int(amm.peg_multiplier), 
-        OracleSource.Pyth(), 
-    )
-
-    # update durations
-    await clearing_house.update_auction_duration(0, 0)
-    await clearing_house.update_lp_cooldown_time(0, 0)
-
-    return usdc_mint, oracle
-
-init_reserves = 20 * 1e13
-init_market = SimulationMarket(
-    market_index=0,
-    amm=SimulationAMM(
-        oracle=None,
-        base_asset_reserve=init_reserves, 
-        quote_asset_reserve=init_reserves, 
-        funding_period = 60 * 60, # 1 hour dont worry about funding updates for now 
-        peg_multiplier=1 * PEG_PRECISION,
-    )
-)
-usdc_mint, oracle = await setup(init_market)
-
-#%%
-## trader
-deposit_amount = 1_000_000_000 * QUOTE_PRECISION
-
+import pprint
 async def view_logs(sig):
     from solana.rpc import commitment
     provider.connection._commitment = commitment.Confirmed 
@@ -91,9 +42,11 @@ async def view_logs(sig):
         logs = (await provider.connection.get_transaction(sig))["result"]["meta"]["logMessages"]
     finally:
         provider.connection._commitment = commitment.Processed 
-    print(logs)
+    pprint.pprint(logs)
 
-async def setup_new_user():
+async def setup_new_user(
+    deposit_amount = 1_000_000_000 * QUOTE_PRECISION
+):
     print('init user...')
     user_keypair = await _setup_user(provider) 
     user_clearing_house = SDKClearingHouse(program, user_keypair)
@@ -106,25 +59,96 @@ async def setup_new_user():
     await user_clearing_house.intialize_user()
     await user_clearing_house.deposit(deposit_amount, 0, usdc_kp.public_key)
 
-    return user_clearing_house
+    return user_clearing_house, deposit_amount
 
 #%%
+# run `anchor localnet` in v2 dir first 
+path = '../driftpy/protocol-v2'
+path = '/Users/brennan/Documents/drift/protocol-v2'
+workspace = create_workspace(path)
+program: Program = workspace["clearing_house"]
+oracle_program: Program = workspace["pyth"]
+provider: Provider = program.provider
+
+async def setup_bank():
+    # init usdc mint
+    usdc_mint = await _usdc_mint(provider)
+
+    # init state + bank + market 
+    clearing_house = Admin(program)
+    await clearing_house.initialize(usdc_mint.public_key, True)
+    await clearing_house.initialize_bank(usdc_mint.public_key)
+
+    return clearing_house, usdc_mint
+
+clearing_house, usdc_mint = await setup_bank()
+
+#%%
+async def setup_market(
+    clearing_house: Admin,
+    market: SimulationMarket
+):
+    amm = market.amm
+    # init_price = round(amm.peg_multiplier / np.log10(PEG_PRECISION), 3)
+    # oracle = await mock_oracle(workspace["pyth"], init_price, -int(np.log10(PEG_PRECISION)))
+
+    oracle = await mock_oracle(workspace["pyth"], 1, -7)
+
+    await clearing_house.initialize_market(
+        oracle, 
+        int(amm.base_asset_reserve), 
+        int(amm.quote_asset_reserve), 
+        int(amm.funding_period), 
+        int(amm.peg_multiplier), 
+        OracleSource.Pyth(), 
+    )
+
+    # update durations
+    await clearing_house.update_auction_duration(0, 0)
+    await clearing_house.update_lp_cooldown_time(0, 0)
+    await clearing_house.update_max_base_asset_amount_ratio(1, 0)
+
+    return oracle
+
+init_reserves = 200 * 1e13
+init_market = SimulationMarket(
+    market_index=0,
+    amm=SimulationAMM(
+        oracle=None,
+        base_asset_reserve=init_reserves, 
+        quote_asset_reserve=init_reserves, 
+        funding_period = 60 * 60, # 1 hour dont worry about funding updates for now 
+        peg_multiplier=1 * PEG_PRECISION,
+    )
+)
+oracle = await setup_market(clearing_house, init_market)
+
+#%%
+## trader
+init_collateral = 2 * 1_000_000_000 * QUOTE_PRECISION
 trader_ch = await setup_new_user()
 lp_ch = await setup_new_user()
 
 #%%
-lp_tokens = int(200 * 1e13)
+lp_tokens = int(100 * 1e13)
 sig = await lp_ch.add_liquidity(
     lp_tokens, 
     0
+)
+lp = await get_user_account(
+    program, lp_ch.authority
+)
+print(
+    lp.positions[0].lp_shares
 )
 lp_ratio = lp_tokens / (init_reserves + lp_tokens)
 lp_ratio
 
 #%%
-baa = int(10 * 1e13)
+baa = int(124 * 1e13)
 direction = PositionDirection.LONG()
-market = await get_market_account(program, 0)
+# market = await get_market_account(program, 0)
+market = init_market
 
 await adjust_oracle_pretrade(
     baa, 
@@ -133,13 +157,23 @@ await adjust_oracle_pretrade(
     oracle,
     oracle_program
 )
-
-await trader_ch.open_position(
+sig = await trader_ch.open_position(
     direction, 
     baa,
     0, 
+    int(100 * 1e13)
+)
+await view_logs(sig)
+
+#%%
+trader = await get_user_account(program, trader_ch.authority)
+print(
+    "trader pos"
+    trader.positions[0].base_asset_amount, 
+    trader.positions[0].quote_asset_amount
 )
 
+#%%
 #%%
 sig = await lp_ch.remove_liquidity(lp_tokens, 0)
 await view_logs(sig)
@@ -158,7 +192,6 @@ lp = await get_user_account(
     lp_ch.authority
 )
 
-init_collateral = deposit_amount * 2
 total_collateral = 0
 
 # users
